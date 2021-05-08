ebf8b54613b0f271479e4c9a732d8a15ec829dda
Dhanushka Chandimal <dhanushkachandimal11@gmail.com>
2021-05-08 07:24:43 +0530

    Create a testing criteria for all situations

--- Singly Linked List/src/SinglyLinkedListTest.java
+++ Singly Linked List/src/SinglyLinkedListTest.java
@@ -3,33 +3,61 @@ import ds.SinglyLinkedList;
 public class SinglyLinkedListTest {
     public static void main(String[] args) {
         SinglyLinkedList singlyLinkedList=new SinglyLinkedList();
-        System.out.println(singlyLinkedList.contains(60));
+
+        System.out.println("------------------------- Test for null linked list - start");
+        System.out.print("Linked List Items : ");
         singlyLinkedList.print();
-        singlyLinkedList.add(0,10);
+        System.out.println("Linked List size = "+singlyLinkedList.size());
+        System.out.println("Is contains 10 --> "+singlyLinkedList.contains(10));
+        System.out.println("Is empty : "+singlyLinkedList.empty());
+        //System.out.println("Get 1st item : "+singlyLinkedList.get(0));
+        //singlyLinkedList.remove(0);
+        System.out.println("------------------------- Test for null linked list - end\n");
+
+        System.out.println("------------------------- Test adding to linked list - start");
+        System.out.print("Linked List Items : ");
+        singlyLinkedList.print();
+        singlyLinkedList.add(10);
+        System.out.print("Linked List Items : ");
         singlyLinkedList.print();
         singlyLinkedList.add(20);
+        System.out.print("Linked List Items : ");
         singlyLinkedList.print();
         singlyLinkedList.add(30);
+        System.out.print("Linked List Items : ");
         singlyLinkedList.print();
-        singlyLinkedList.add(40);
+        singlyLinkedList.add(0,5);
+        System.out.print("Linked List Items : ");
         singlyLinkedList.print();
-        singlyLinkedList.add(50);
+        singlyLinkedList.add(3,25);
+        System.out.print("Linked List Items : ");
         singlyLinkedList.print();
-        singlyLinkedList.add(2,25);
-        singlyLinkedList.print();
-        singlyLinkedList.remove(2);
-        singlyLinkedList.print();
-        System.out.println(singlyLinkedList.get(0));
-        System.out.println(singlyLinkedList.get(1));
-        System.out.println(singlyLinkedList.get(2));
-        System.out.println(singlyLinkedList.get(3));
+        System.out.println("Linked List size = "+singlyLinkedList.size());
+        System.out.println("Is contains 10 --> "+singlyLinkedList.contains(10));
+        System.out.println("Is contains 55 --> "+singlyLinkedList.contains(55));
+        System.out.println("Is empty : "+singlyLinkedList.empty());
+        System.out.println("1st item : "+singlyLinkedList.get(0));
+        System.out.println("3rd item : "+singlyLinkedList.get(2));
+        System.out.println("4th item : "+singlyLinkedList.get(3));
+        System.out.println("------------------------- Test adding to linked list - end\n");
 
-        System.out.println(singlyLinkedList.contains(10));
-        System.out.println(singlyLinkedList.contains(20));
-        System.out.println(singlyLinkedList.contains(30));
-        System.out.println(singlyLinkedList.contains(45));
-        System.out.println(singlyLinkedList.contains(50));
-        System.out.println(singlyLinkedList.contains(60));
-        System.out.println(singlyLinkedList.size());
+        System.out.println("------------------------- Test remove from linked list - start");
+        System.out.print("Linked List Items : ");
+        singlyLinkedList.print();
+        singlyLinkedList.remove(3);
+        System.out.print("Linked List Items : ");
+        singlyLinkedList.print();
+        singlyLinkedList.remove(0);
+        System.out.print("Linked List Items : ");
+        singlyLinkedList.print();
+        System.out.println("------------------------- Test remove from linked list - end\n");
+
+        System.out.println("------------------------- Test clear() method - start");
+        System.out.print("Linked List Items before clear : ");
+        singlyLinkedList.print();
+        singlyLinkedList.clear();
+        System.out.print("Linked List Items after clear : ");
+        singlyLinkedList.print();
+        System.out.println("------------------------- Test clear() method - end");
     }
 }
